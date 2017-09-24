1359192a3ce23044cc545be845b18a5573bc9a94
xyyx <xyyx@mail.ru>
2017-09-25 00:18:17 +0300

    MTP Dirty Hack (use mtp as default after connect to usb) (3/3)

--- config/common.mk
+++ config/common.mk
@@ -208,6 +208,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.storage_manager.enabled=true
 
+# Needs for MTP Dirty Hack
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
